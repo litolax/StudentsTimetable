9e0e4f362ff1aee0bef24d45814f18d1e4d2cc14
Yuri Ratkevich <ura_rat@mail.ru>
2022-09-16 00:51:26 +0300

    Added index.js for gh pages test

---
+++ index.js
@@ -0,0 +1,16 @@
+const socket = new WebSocket("ws://s2.zloserver.com:41065/healthCheck/students/bot");
+let elem = document.getElementById('text')
+
+socket.onmessage = (event) => {
+    elem.textContent = "Hello, I'm a bot for Students and I'm " + (event.data.split(':')[1] ? "Alive" : "Dead")
+}
+
+socket.onopen = () => {
+    setInterval(() => {
+        socket.send("ping");
+    }, 1000)
+}
+
+socket.onclose = () => {
+    elem.textContent = "Hello, I'm a bot for Students and I'm Dead"
+}
