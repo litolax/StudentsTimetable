431c5e029bd7d28dc5b46f7650dc0135b387a55f
Yuri Ratkevich <ura_rat@mail.ru>
2023-03-22 18:53:25 +0300

    Disable all chromedriver logs

--- StudentsTimetable/Services/Utils.cs
+++ StudentsTimetable/Services/Utils.cs
@@ -48,12 +48,26 @@ public static class Utils
     public static ChromeDriver CreateChromeDriver()
     {
         var service = ChromeDriverService.CreateDefaultService();
-        service.SuppressInitialDiagnosticInformation = true;
         
-        var options = new ChromeOptions();
-        options.AddArgument("headless");
+        service.EnableVerboseLogging = false;
+        service.SuppressInitialDiagnosticInformation = true;
+        service.HideCommandPromptWindow = true;
+        
+        var options = new ChromeOptions
+        {
+            PageLoadStrategy = PageLoadStrategy.Normal
+        };
+
         options.AddArgument("--no-sandbox");
-        options.AddArguments("--disable-dev-shm-usage");
+        options.AddArgument("--headless");
+        options.AddArgument("--disable-gpu");
+        options.AddArgument("--disable-crash-reporter");
+        options.AddArgument("--disable-extensions");
+        options.AddArgument("--disable-in-process-stack-traces");
+        options.AddArgument("--disable-logging");
+        options.AddArgument("--disable-dev-shm-usage");
+        options.AddArgument("--log-level=3");
+        options.AddArgument("--output=/dev/null");
 
         return new ChromeDriver(service, options);
     }
