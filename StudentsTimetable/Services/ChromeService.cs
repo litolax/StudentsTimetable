16b8f043abd4db03424b7468da224142cc8ee9c6
litolax <yuri.ratkevich85360@gmail.com>
2023-08-29 02:07:45 +0300

    Fix firefox creation #3

--- StudentsTimetable/Services/ChromeService.cs
+++ StudentsTimetable/Services/ChromeService.cs
@@ -23,16 +23,27 @@ public class ChromeService : IChromeService
         service.SuppressInitialDiagnosticInformation = true;
         service.HideCommandPromptWindow = true;
 
-        var options = new FirefoxOptions();
+        var options = new FirefoxOptions
+        {
+            PageLoadStrategy = PageLoadStrategy.Eager
+        };
 
         options.AddArgument("--no-sandbox");
         options.AddArgument("no-sandbox");
         options.AddArgument("--headless");
+        options.AddArgument("--disable-gpu");
+        options.AddArgument("--disable-crash-reporter");
+        options.AddArgument("--disable-extensions");
+        options.AddArgument("--disable-in-process-stack-traces");
+        options.AddArgument("--disable-logging");
+        options.AddArgument("--disable-dev-shm-usage");
         options.AddArgument("--log-level=3");
+        options.AddArgument("--output=/dev/null");
         options.AddArgument("--force-device-scale-factor=1");
+        options.AddArgument("--disable-browser-side-navigation");
         options.SetEnvironmentVariable("webdriver.gecko.driver", "./geckodriver");
         
-        var driver = new FirefoxDriver("./", options,TimeSpan.FromMinutes(2));
+        var driver = new FirefoxDriver(service, options,TimeSpan.FromMinutes(2));
         driver.Manage().Timeouts().PageLoad.Add(TimeSpan.FromMinutes(2));
 
         return driver;
