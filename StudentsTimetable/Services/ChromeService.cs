67b27126ec6a85751ca8d73b8ad4b3dfa05f9dbc
litolax <yuri.ratkevich85360@gmail.com>
2023-08-29 01:48:05 +0300

    Fix firefox creation

--- StudentsTimetable/Services/ChromeService.cs
+++ StudentsTimetable/Services/ChromeService.cs
@@ -23,25 +23,14 @@ public class ChromeService : IChromeService
         service.SuppressInitialDiagnosticInformation = true;
         service.HideCommandPromptWindow = true;
 
-        var options = new FirefoxOptions
-        {
-            PageLoadStrategy = PageLoadStrategy.Eager
-        };
+        var options = new FirefoxOptions();
 
         options.AddArgument("--no-sandbox");
         options.AddArgument("--headless");
-        options.AddArgument("--disable-gpu");
-        options.AddArgument("--disable-crash-reporter");
-        options.AddArgument("--disable-extensions");
-        options.AddArgument("--disable-in-process-stack-traces");
-        options.AddArgument("--disable-logging");
-        options.AddArgument("--disable-dev-shm-usage");
         options.AddArgument("--log-level=3");
-        options.AddArgument("--output=/dev/null");
         options.AddArgument("--force-device-scale-factor=1");
-        options.AddArgument("--disable-browser-side-navigation");
         
-        var driver = new FirefoxDriver(service, options,TimeSpan.FromSeconds(180));
+        var driver = new FirefoxDriver("./", options,TimeSpan.FromSeconds(120));
         driver.Manage().Timeouts().PageLoad += TimeSpan.FromMinutes(3);
 
         return driver;
